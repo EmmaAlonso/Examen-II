ee6b3086f6ea93218044e0a09da67647a0a09565
Emmanuel Alonso <emmanuel010205@gmail.com>
2025-03-11 21:07:35 -0600

    Nuevos Cambios

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import StudentTable from "app/components/studentTable";
+
+
+export default function Home() {
+  return (
+
+    /* Lo que esta pasando es que importe el componente StudenTable y bajo de return puse StudenTable para que reconiera la importacacion
+    ya que en este tengo las tablas y las opcciones de todo en la pagina principal */
+
+    <>
+      <h1 className="text-4xl text-center mt-8">
+        ¿Crees poder lograrlo? Si no confias en ti, quien lo hará?
+      </h1>
+      <StudentTable/>
+      <h2 className="text-2xl text-center mt-4">
+        Aquí debe de verse tu tabla, ¿cuál tabla? Lee el archivo README
+      </h2>
+    </>
+  );
+}
